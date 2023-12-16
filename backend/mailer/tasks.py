d0f9e98ba5cf0e98558e4a2d60ec817d6010c16f
AlexDyakonov <shram.monolit@mail.ru>
2023-12-16 19:33:23 +0300

    Fixed mail html show. TODO fix token_email

--- backend/mailer/tasks.py
+++ backend/mailer/tasks.py
@@ -2,7 +2,8 @@ from celery import shared_task
 from django.core.mail import send_mail
 from .serializers import MailSerializer
 from django.conf import settings
-
+from bs4 import BeautifulSoup
+from django.template.loader import render_to_string
 
 @shared_task
 def bar():
@@ -11,12 +12,15 @@ def bar():
 
 @shared_task
 def send_mailing(emails: list, mail: dict):
-    print(emails)
-    print(mail)
+    html_message = mail.get("html_message")
+    soup = BeautifulSoup(html_message, 'html.parser')
+    html_message = str(soup)
+    print(html_message)
+    
     send_mail(
         mail.get("subject"),
         mail.get("message"),
         settings.EMAIL_HOST_USER,
         emails,
-        mail.get("html_message"),
+        html_message=html_message,
     )
--- backend/utils/email/email_utils.py
+++ backend/utils/email/email_utils.py
@@ -6,5 +6,5 @@ from django.utils.translation import activate
 def send_token_email(user, confirmation_link):
     html_message = render_to_string('token_email.html', {'confirmation_link': confirmation_link})
     subject = ('Подтверждение регистрации')
-    message = ('ля завершения регистрации, пожалуйста, перейдите по следующей ссылке:') + str(confirmation_link)
+    message = ('Для завершения регистрации, пожалуйста, перейдите по следующей ссылке:') + str(confirmation_link)
     send_mail(subject, message, settings.EMAIL_HOST_USER , [user.email], html_message=html_message)
