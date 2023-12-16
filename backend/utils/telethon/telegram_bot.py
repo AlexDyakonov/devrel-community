0a0cef011317a602b86cd267912cbd5ed8cf0605
AlexDyakonov <shram.monolit@mail.ru>
2023-12-17 02:35:17 +0300

    Small easter egg

--- backend/utils/telethon/telegram_bot.py
+++ backend/utils/telethon/telegram_bot.py
@@ -12,7 +12,49 @@ logging.basicConfig(level=logging.INFO)
 api_id = int(os.getenv('TELETHON_API_ID'))
 api_hash = os.getenv('TELETHON_API_HASH')
 
+function_calls = {}
 
+
+def log_first_call(func):
+    def wrapper(*args, **kwargs):
+        function_name = func.__name__
+        if function_name not in function_calls:
+            logging.info(
+                """
+                
+                * g o a t s e x * g o a t s e x * g o a t s e x *
+                g                                               g  
+                o /     \             \            /    \       o
+                a|       |             \          |      |      a
+                t|       `.             |         |       :     t
+                s`        |             |        \|       |     s
+                e \       | /       /  \\\   --__ \\       :    e
+                x  \      \/   _--~~          ~--__| \     |    x  
+                *   \      \_-~                    ~-_\    |    *
+                g    \_     \        _.--------.______\|   |    g
+                o      \     \______// _ ___ _ (_(__>  \   |    o
+                a       \   .  C ___)  ______ (_(____>  |  /    a
+                t       /\ |   C ____)/      \ (_____>  |_/     t
+                s      / /\|   C_____)       |  (___>   /  \    s
+                e     |   (   _C_____)\______/  // _/ /     \   e
+                x     |    \  |__   \\_________// (__/       |  x
+                *    | \    \____)   `----   --'             |  *
+                g    |  \_          ___\       /_          _/ | g
+                o   |              /    |     |  \            | o
+                a   |             |    /       \  \           | a
+                t   |          / /    |         |  \           |t
+                s   |         / /      \__/\___/    |          |s
+                e  |           /        |    |       |         |e
+                x  |          |         |    |       |         |x
+                * g o a t s e x * g o a t s e x * g o a t s e x *
+                """)
+            function_calls[function_name] = True
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@log_first_call
 async def get_user_id(user_name: str):
     try:
         client = TelegramClient('anon', api_id, api_hash)
@@ -24,6 +66,8 @@ async def get_user_id(user_name: str):
         logging.error(f"Error in get_user_id: {e}")
         return None
 
+
+@log_first_call
 async def send_msg(user_id, msg):
     try:
         client = TelegramClient('anon', api_id, api_hash)
