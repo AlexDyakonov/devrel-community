cb2159cc69c2499c6a1a79fc12eb6029907708b4
AlexDyakonov <shram.monolit@mail.ru>
2023-12-16 07:37:39 +0300

    Added user-by-token method to get user data for form

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path, re_path
-from .views import EventList, EventDetail, FormFieldsListCreateView, FormFieldsDetailView
+from .views import EventList, EventDetail, FormFieldsListCreateView, FormFieldsDetailView, UserByTokenAPIView
 
 urlpatterns = [
     path('events/', EventList.as_view(), name='event-list'),
     path('events/<int:pk>/', EventDetail.as_view(), name='event-detail'),
     path('formfields/', FormFieldsListCreateView.as_view(), name='formfields-list-create'),
     path('formfields/<int:pk>/', FormFieldsDetailView.as_view(), name='formfields-detail'),
-]
+    path('user-by-token/', UserByTokenAPIView.as_view(), name='user-by-token'),
+    ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Event, FormFields
 from .serializers import EventSerializer, FormFieldsSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from users.serializers import UserSerializer
+from rest_framework.exceptions import NotFound
+
 # Create your views here.
 
 
@@ -20,3 +26,20 @@ class FormFieldsListCreateView(generics.ListCreateAPIView):
 class FormFieldsDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = FormFields.objects.all()
     serializer_class = FormFieldsSerializer
+
+class UserByTokenAPIView(generics.RetrieveAPIView):
+    def get(self, request, format=None):
+        token_key = request.GET.get('token', None)
+
+        if not token_key:
+            return Response({"error": "Token is required"}, status=400)
+
+        try:
+            token = Token.objects.get(key=token_key)
+        except Token.DoesNotExist:
+            raise NotFound("Token not found")
+
+        user = token.user
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data)
