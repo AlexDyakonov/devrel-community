33acf8f37510be2ed1314dd12d94593a4187abc4
Vaniog <forvantar@gmail.com>
2023-12-17 03:50:08 +0300

    Refflinks quick fix

--- backend/refflink/urls.py
+++ backend/refflink/urls.py
@@ -3,6 +3,6 @@ from .views import ReffLinkListView, ReffLinkDetailView, ReffLinkRedirectView
 
 urlpatterns = [
     path('manage/', ReffLinkListView.as_view(), name='refflink-list'),
-    path('manage/<int:pk>/', ReffLinkDetailView.as_view(), name='refflink-detail'),
+    path('manage/<str:pk>/', ReffLinkDetailView.as_view(), name='refflink-detail'),
     path('go/<str:pk>', ReffLinkRedirectView.as_view(), name='redirect-ref')
 ]
