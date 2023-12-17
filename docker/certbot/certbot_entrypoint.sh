a04919450da3b79e5be75c1aba3495309fea00c1
AlexDyakonov <shram.monolit@mail.ru>
2023-12-17 16:25:47 +0300

    Attempt to fix SSL

--- docker/certbot/certbot_entrypoint.sh
+++ docker/certbot/certbot_entrypoint.sh
@@ -2,9 +2,9 @@
 
 trap exit TERM
 
-DOMAIN=$(echo $ALLOWED_HOST | cut -d ',' -f1) # Если ALLOWED_HOST содержит несколько доменов, разделенных запятой, берем первый
+# DOMAIN=$(echo $ALLOWED_HOST | cut -d ',' -f1) # Если ALLOWED_HOST содержит несколько доменов, разделенных запятой, берем первый
 
-certbot certonly --webroot --webroot-path=/var/www/certbot --register-unsafely-without-email --agree-tos -d $DOMAIN --quiet
+certbot certonly --webroot --webroot-path=/var/www/certbot --register-unsafely-without-email --agree-tos -d shampiniony.ru --quiet
 
 while :; do 
   certbot renew --webroot --webroot-path=/var/www/certbot --quiet
