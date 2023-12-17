655996e2ee43c6cad57477e4c5f25158bb1cc435
AlexDyakonov <shram.monolit@mail.ru>
2023-12-17 13:50:20 +0300

    Added certbot. TODO test

---
+++ docker/certbot/certbot_entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+trap exit TERM
+
+DOMAIN=$(echo $ALLOWED_HOST | cut -d ',' -f1) # Если ALLOWED_HOST содержит несколько доменов, разделенных запятой, берем первый
+
+certbot certonly --webroot --webroot-path=/var/www/certbot --register-unsafely-without-email -d $DOMAIN --quiet
+while :; do 
+  certbot renew --webroot --webroot-path=/var/www/certbot --quiet
+  sleep 12h & wait $${!}
+done
